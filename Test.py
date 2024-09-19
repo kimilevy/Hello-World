6d621004b3df9f69e01e0c4010c503e7d452e01a
kim <kimlevy06@gmail.com>
2024-09-20 02:06:17 +0300

    add

--- Test.py
+++ Test.py
@@ -19,5 +19,10 @@ def initialize_db():
             )
         ''')
         cursor.execute
-
+def get_first_run_count():
+    with get_db_connection() as conn:
+        cursor = conn.execute('SELECT count FROM run_counter LIMIT 1')
+        result = cursor.fetchone()  # Retrieve the first row
+        return result[0] if result else None  # Return the count or None if no result
+        
 AWS_SECRET_ACCESS_KEY='wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
